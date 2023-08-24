680d5342193b34a894e6af354d8b63201aff059c
Ilya Rodionov <ris58h@gmail.com>
2023-08-24 13:08:25 +0700

    Initial commit

---
+++ core.js
@@ -0,0 +1,133 @@
+export class Point {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    distanceTo(point) {
+        const dx = point.x - this.x
+        const dy = point.y - this.y
+        return Math.sqrt((dx * dx) + (dy * dy))
+    }
+}
+
+export class Generator extends Point {
+    constructor(x, y, weight) {
+        super(x, y)
+        this.weight = weight
+    }
+}
+
+export class Line {
+    constructor(begin, end) {
+        this.begin = begin
+        this.end = end
+    }
+
+    distanceTo(point) {
+        return Line.distanceTo(this.begin, this.end, point)
+    }
+
+    static distanceTo(begin, end, point) {
+        const x0 = point.x
+        const y0 = point.y
+        const x1 = begin.x
+        const y1 = begin.y
+        const x2 = end.x
+        const y2 = end.y
+        const ed = euclidianDistance(x1, y1, x2, y2)
+        return Math.abs((y2 - y1)*x0 - (x2 - x1)*y0 + x2*y1 - y2*x1) / ed
+    }
+}
+
+export class Polygon {
+    constructor(vertices) {
+        this.vertices = vertices
+    }
+
+    area() {
+        let area
+        processFacets((begin, end) => {
+            const ik = begin.x
+            const jk = begin.y
+            const ik1 = end.x
+            const jk1 = end.y
+            area += ik * jk1 - ik1 * jk
+
+            return true
+        })
+        return Math.abs(0.5 * area)
+    }
+
+    centroid() {
+        let ax
+        let ay
+        let area
+        processFacets((begin, end) => {
+            const xk = begin.x
+            const yk = begin.y
+            const xk1 = end.x
+            const yk1 = end.y
+
+            const shared = xk * yk1 - xk1 * yk
+            area += shared
+            ax += (xk + xk1) * shared
+            ay += (yk + yk1) * shared
+
+            return true
+        })
+        area *= 0.5
+        return new Point(ax / (6 * area), ay / (6 * area))
+    }
+
+    contains(point) {
+        let result = false
+        processFacets((begin, end) => {
+            const p_x = point.x
+            const p_y = point.y
+            const begin_x = begin.x
+            const begin_y = begin.y
+            const end_x = end.x
+            const end_y = end.y
+
+            if ((p_x == begin_x && p_y == begin_y) || (p_x == end_x && p_y == end_y)) {
+                result = true
+                return false
+            }
+
+            if ( ((end_y > p_y) != (begin_y > p_y)) && (p_x < (begin_x - end_x) * (p_y - end_y) / (begin_y - end_y) + end_x) ){
+                result = !result
+            }
+
+            return true
+        })
+        return result
+    }
+
+    processFacets(callback) {
+        const iterator = this.vertices[Symbol.iterator]()
+        const first = iterator.next().value
+        let begin = first
+        let end
+        let interrupted
+        do {
+            const next = iterator.next()
+            end = next.value
+            interrupted = !callback(begin, end)
+            if (interrupted) {
+                break
+            }
+            begin = end
+        } while (!next.done)
+        if (!interrupted) {
+            interrupted = !callback(begin, first)
+        }
+    }
+}
+
+export class Cell {
+    constructor(generator, polygon) {
+        this.generator = generator
+        this.polygon = polygon
+    }
+}
---
+++ power-diagram.js
@@ -0,0 +1,185 @@
+import {Cell, Generator, Line, Point, Polygon} from "./core"
+
+export function calculate(boundPolygon, weightedPoints) {
+    const length = generators.size
+    if (length == 0) {
+        return []
+    }
+
+    const generators = normalizedGenerators(weightedPoints)
+    const cellBounds = Array(length).fill(boundPolygon)
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = i + 1; j < length; j++) {
+            const divider = divisionLine(generators[i], generators[j])
+            cellBounds[i] = cut(generators[i], cellBounds[i], divider)
+            cellBounds[j] = cut(generators[j], cellBounds[j], divider)
+        }
+    }
+
+    const cells = []
+    for (let i = 0; i < length; i++) {
+        cells[i] = new Cell(generators[i], cellBounds[i])
+    }
+    return cells
+}
+
+function divisionLine(g_i, g_j) {
+    const x_i = g_i.x
+    const y_i = g_i.y
+    const w_i = g_i.weight
+    const x_j = g_j.x
+    const y_j = g_j.y
+    const w_j = g_j.weight
+
+    const dx = x_i - x_j
+    const dy = y_i - y_j
+    const bb = ((x_i * x_i - x_j * x_j) + (y_i * y_i - y_j * y_j) - (w_i * w_i - w_j * w_j)) / 2
+    function f_x() {
+        const k = -(dy / dx)
+        const b = bb / dx
+        const x = k * y + b
+        return x
+    }
+    function f_y() {
+        const k = -(dx / dy)
+        const b = bb / dy
+        const y = k * x + b
+        return y
+    }
+
+    let x1
+    let y1
+    let x2
+    let y2
+    if (y_i == y_j) {
+        y1 = 0
+        x1 = f_x(y1)
+        y2 = 1
+        x2 = f_x(y2)
+    } else {
+        x1 = 0
+        y1 = f_y(x1)
+        x2 = 1
+        y2 = f_y(x2)
+    }
+
+    return new Line(new Point(x1, y1), new Point(x2, y2));
+}
+
+function intersectionOfFacetAndLine(facet, line) {
+    const intersectionPoint = intersection(facet, line)
+    if (intersectionPoint == null) {
+        return null
+    }
+    const intX = intersectionPoint.x
+    const intY = intersectionPoint.y
+    const onFacetX = (Math.min(facet.begin.x, facet.end.x) <= intX) && (intX <= Math.max(facet.begin.x, facet.end.x))
+    const onFacetY = (Math.min(facet.begin.y, facet.end.y) <= intY) && (intY <= Math.max(facet.begin.y, facet.end.y))
+    if (onFacetX && onFacetY) {
+        return new Point(intX, intY)
+    }
+    return null
+}
+
+function intersection(l1, l2) {
+    const denom = ((l2.end.y - l2.begin.y) * (l1.end.x - l1.begin.x)) -
+            ((l2.end.x - l2.begin.x) * (l1.end.y - l1.begin.y))
+
+    const numea = ((l2.end.x - l2.begin.x) * (l1.begin.y - l2.begin.y)) -
+            ((l2.end.y - l2.begin.y) * (l1.begin.x - l2.begin.x))
+
+    const numeb = ((l1.end.x - l1.begin.x) * (l1.begin.y - l2.begin.y)) -
+            ((l1.end.y - l1.begin.y) * (l1.begin.x - l2.begin.x))
+
+    if (denom == 0.0) {
+        //TODO
+        if (numea == 0.0 && numeb == 0.0) {
+            return null
+        }
+        return null
+    }
+
+    const ua = numea / denom
+    const ub = numeb / denom//TODO not used?
+
+    const intX = l1.begin.x + ua * (l1.end.x - l1.begin.x)
+    const intY = l1.begin.y + ua * (l1.end.y - l1.begin.y)
+
+    return new Point(intX, intY)
+}
+
+function cut(generator, polygon, line) {
+    const newVertices = []
+    polygon.processFacets((begin, end) => {
+        if (notOnOppositeSides(generator, begin, line)) {
+            newVertices.add(begin)
+        }
+        const facet = new Line(begin, end)
+        const intersectionPoint = intersectionOfFacetAndLine(facet, line)
+        if (intersectionPoint != null) {
+            newVertices.add(intersectionPoint)
+        }
+        return true
+    })
+    reduceVertices(newVertices)
+    const newSize = newVertices.size()
+    if (newSize < 3) {
+        throw `Illegal state`
+    }
+    const newPolygon = new Polygon(newVertices)
+    if (!newPolygon.contains(generator)) {
+        throw 'Illegal state'
+    }
+    return newPolygon
+}
+
+//TODO naming
+function notOnOppositeSides(p1, p2, l) {
+    const ax = p1.x
+    const ay = p1.y
+    const bx = p2.x
+    const by = p2.y
+    const x1 = l.begin.x
+    const y1 = l.begin.y
+    const x2 = l.end.x
+    const y2 = l.end.y
+    return ((y1 - y2) * (ax - x1) + (x2 - x1) * (ay - y1)) * ((y1 - y2) * (bx - x1) + (x2 - x1) * (by - y1)) >= 0
+}
+
+function reduceVertices(points) {
+    let prev = null
+    const iterator = points[Symbol.iterator]()
+    let next
+    while (!(next = iterator.next()).done) {
+        const point = next.value
+        if (prev != null && tooClose(prev, point)) {
+            iterator.remove()
+        }
+        prev = point
+    }
+}
+
+const EPS = 0.01
+
+function tooClose(p1, p2) {
+    return p1.distanceTo(p2) < EPS
+}
+
+function normalizedGenerators(generators) {
+    let minK = Float.MAX_VALUE
+    for (let i = 0; i < generators.size - 1; i++) {
+        for (let j = i + 1; j < generators.size; j++) {
+            let dist = generators[i].distanceTo(generators[j])
+            let ws = pw[i] + pw[j]
+            if (Float.isInfinite(ws)) {//TODO
+                ws = Float.MAX_VALUE
+            }
+            let k = dist / ws
+            minK = Math.min(minK, k)
+        }
+    }
+
+    return generators.map(generator => {
+        new Generator(generator.x, generator.y, generator.weight * minK)
+    })
+}
